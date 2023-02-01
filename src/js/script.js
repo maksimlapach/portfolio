cffc4dbed22e9fdde173993545622d55ce40d75a
Maksym <maksimlapach@gmail.com>
2023-02-01 14:49:31 +0200

    Finished third block v0.3.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeMenu = document.querySelector('.menu__close');
+      closeMenu = document.querySelector('.menu__close'),
+      overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 closeMenu.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
     menu.classList.remove('active');
 });
